d3b2944b17f1d9af4f1ca6da22a0fe21e234187e
avanishihora48 <163129933+avanishihora48@users.noreply.github.com>
2024-09-01 17:33:11 +0530

    commit

---
+++ Core_Java/Multithreading/Ans2.java
@@ -0,0 +1,30 @@
+package Multithreading;
+
+class A1 extends Thread
+{
+	@Override
+	public void run() 
+	{
+		System.out.println("A1 thread running...");
+	}
+}
+
+class A2 extends Thread
+{
+	@Override
+	public void run() 
+	{
+		System.out.println("A2 thread running...");
+	}
+}
+public class Ans2 
+{
+     public static void main(String[] args) 
+     {
+		 A1 a1 = new A1();
+		 A2 a2 = new A2();
+		 
+		 a1.start();
+		 a2.start();
+	 }
+}
